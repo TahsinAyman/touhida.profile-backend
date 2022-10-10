6858e0a26eb823d760c2a531ce1c1006423cb175
Tahsin Ayman <87863787+TahsinAyman@users.noreply.github.com>
2022-10-10 01:25:06 +0000

    Heroku hosting

--- Profile/urls.py
+++ Profile/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve 
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +32,7 @@ urlpatterns = [
     path('api/v1/achivements/', include("achivements.urls")),
     path('api/v1/comments/', include("comments.urls")),
     path('api/v1/bio/', include("bio.urls")),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
